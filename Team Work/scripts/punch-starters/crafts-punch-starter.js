e504c896d55f51df31fa413bec7be2fd90b1cad9
Nikolay <rogchev@gmail.com>
2016-11-10 15:13:25 +0200

    food-punch-starter and crafts-punch-starter classes implemented

--- Team Work/scripts/punch-starters/crafts-punch-starter.js
+++ Team Work/scripts/punch-starters/crafts-punch-starter.js
@@ -1,5 +1,25 @@
-class CraftsPunchStarter {
-	//TODO: Implement CraftsPunchStarter
+let BasePunchStarter = require('./base-punch-starter.js');
+
+class CraftsPunchStarter extends BasePunchStarter{
+    constructor(id, name, manufacturer, description, genres, targetPrice, resources) {
+        super(id, name, manufacturer, description, genres, targetPrice);
+        this._resources = validateResources(resources);
+    }
+    get resources() {
+        return this._resources;
+    }
+}
+
+function validateResources(resources) {
+    if (resources.constructor.name === 'Array') {
+        for (let item of resources) {
+            if (typeof item != 'string') {
+                throw new TypeError('Invalid resources format!');
+            }
+        }
+        return resources;
+    }
+    throw new TypeError("Invalid ingredients format!");
 }
 
 module.exports = CraftsPunchStarter;
--- Team Work/scripts/punch-starters/food-punch-starter.js
+++ Team Work/scripts/punch-starters/food-punch-starter.js
@@ -1,5 +1,37 @@
-class FoodPunchStarter {
-	//TODO: Implement FoodPunchStarter
+let BasePunchStarter = require('./base-punch-starter.js');
+
+class FoodPunchStarter extends BasePunchStarter {
+    constructor(id, name, manufacturer, description, genres, targetPrice, ingredients, recipe) {
+        super(id, name, manufacturer, description, genres, targetPrice);
+        this._ingredients = validateIngredients(ingredients);
+        this._recipe = validateRecipe(recipe);
+    }
+
+    get ingredients(){
+        return this._ingredients;
+    }
+    get recipe() {
+        return this._recipe;
+    }
+}
+
+function validateIngredients(ingredients) {
+    if (ingredients.constructor.name === 'Array') {
+        for (let item of ingredients) {
+            if (typeof item != 'string') {
+                throw new TypeError('Invalid ingredients format!');
+            }
+        }
+        return ingredients;
+    }
+    throw new TypeError("Invalid ingredients format!");
+}
+
+function validateRecipe(recipe) {
+    if (typeof recipe == 'string' && recipe != '') {
+        return recipe;
+    }
+    throw new TypeError("Invalid ingredients format!");
 }
 
 module.exports = FoodPunchStarter;
