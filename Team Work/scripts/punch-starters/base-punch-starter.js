2bf691999042fde2177e22e7b97494eae7d4f747
Kamen <kamenminkov.com@abv.bg>
2016-11-10 15:01:34 +0200

    Base class implemented

--- Team Work/scripts/punch-starters/base-punch-starter.js
+++ Team Work/scripts/punch-starters/base-punch-starter.js
@@ -1,5 +1,76 @@
-class BasePunchStarter {
-    //TODO: Implement BasePunchStarter
+function validateNumber(number) {
+    if (typeof number != 'number'){
+        throw new TypeError
+    }
+    return number
 }
+
+function validateString (str) {
+    if (typeof str != 'string'){
+        throw new TypeError
+    }
+    return str
+}
+
+function validateArray(array) {
+    if (!Array.isArray(array)){
+        throw new TypeError
+    }
+    return array
+}
+
+
+
+class BasePunchStarter {
+    constructor(id, name, manufacturer, description, genres, targetPrice){
+        if (new.target === BasePunchStarter){
+            throw new Error()
+        }
+        this._id = validateNumber(id)
+        this._name = validateString(name)
+        this._manufacturer = validateString(manufacturer)
+        this._description = validateString(description)
+        this._genres = validateArray(genres)
+        this._tagetPrice = validateNumber(targetPrice)
+        this._accumulatedMoney = 0
+    }
+
+    get id() {
+        return this._id
+    }
+
+    get name (){
+        return this._name
+    }
+
+    get manufacturer (){
+        return this._manufacturer
+    }
+
+    get description () {
+        return this._description
+    }
+
+    get genres () {
+        return this._genres
+    }
+
+    get targetPrice (){
+        return this._tagetPrice
+    }
+
+    get accumulatedMoney () {
+        return this._accumulatedMoney
+    }
+
+    set accumulatedMoney (money) {
+        if (typeof money != 'number'){
+            throw new TypeError
+        }
+        this._accumulatedMoney += money
+    }
+}
+
+
 
 module.exports = BasePunchStarter;
