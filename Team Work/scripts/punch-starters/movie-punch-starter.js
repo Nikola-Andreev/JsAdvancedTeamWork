f71aa0d7d0583aa7ae21cb3dd89a77d12896f639
Nikolay <rogchev@gmail.com>
2016-11-10 15:39:21 +0200

    movie-punch-starter class implemented

--- Team Work/scripts/punch-starters/movie-punch-starter.js
+++ Team Work/scripts/punch-starters/movie-punch-starter.js
@@ -1,5 +1,36 @@
-class MoviePunchStarter {
-	//TODO: Implement MoviePunchStarter
+let BasePunchStarter = require('./base-punch-starter.js');
+
+class MoviePunchStarter extends BasePunchStarter{
+    constructor(id, name, manufacturer, description, genres, targetPrice, director, actors) {
+        super(id, name, manufacturer, description, genres, targetPrice);
+        this._director = validateDirector(director);
+        this._actors = validateActors(actors);
+    }
+    get director() {
+        return this._director;
+    }
+    get actors() {
+        return this._actors;
+    }
+}
+
+function validateDirector(director) {
+    if (typeof director == 'string' && director != '') {
+        return director;
+    }
+    throw new TypeError("Invalid director format!");
+}
+
+function validateActors(actors) {
+    if (actors.constructor.name === 'Array') {
+        for (let item of actors) {
+            if (typeof item != 'string') {
+                throw new TypeError('Invalid actors format!');
+            }
+        }
+        return actors;
+    }
+    throw new TypeError("Invalid actors format!");
 }
 
 module.exports = MoviePunchStarter;
