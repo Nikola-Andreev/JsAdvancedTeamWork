d031a1387668b31b9b43fff11b0c7e8953ae10bb
Nikola Andreev <noka@abv.bg>
2016-11-10 15:27:08 +0200

    Implement classes

--- Team Work/scripts/punch-starters/game-punch-starter.js
+++ Team Work/scripts/punch-starters/game-punch-starter.js
@@ -1,5 +1,22 @@
-class GamePunchStarter {
-	//TODO: Implement GamePunchStarter
+let BasePunchStarter = require('./base-punch-starter')
+
+function checkType(param) {
+    if(Object.prototype.toString.call( param ) !== '[object Array]' ){
+        throw new TypeError('The technologiesUsed is invalid')
+    } else {
+        return param
+    }
+}
+
+class GamePunchStarter extends BasePunchStarter{
+    constructor(id, name, manufacturer, description, genres, targetPrice, technologiesUsed){
+        super(id, name, manufacturer, description, genres, targetPrice)
+        this._technologiesUsed = checkType(technologiesUsed)
+    }
+
+    get technologiesUsed(){
+        return this._technologiesUsed
+    }
 }
 
 module.exports = GamePunchStarter;
--- Team Work/scripts/punch-starters/innovative-punch-starter.js
+++ Team Work/scripts/punch-starters/innovative-punch-starter.js
@@ -1,5 +1,9 @@
-class InnovativePunchStarter {
-	//TODO: Implement InnovativePunchStarter
+let BasePunchStarter = require('./base-punch-starter')
+
+class InnovativePunchStarter extends BasePunchStarter{
+    constructor(id, name, manufacturer, description, genres, targetPrice){
+        super(id, name, manufacturer, description, genres, targetPrice)
+    }
 }
 
 module.exports = InnovativePunchStarter;
