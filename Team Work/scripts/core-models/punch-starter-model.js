dbc1cb08e577e543908c09fe36bd143feae2b6f8
Nikola Andreev <noka@abv.bg>
2016-11-11 19:57:25 +0200

    2.	Main Render Functionality - ready

--- Team Work/scripts/core-models/punch-starter-model.js
+++ Team Work/scripts/core-models/punch-starter-model.js
@@ -1,5 +1,22 @@
 class PunchStarterModel {
-	//TODO: Implement Punch Starter Model
+	render(punchStarter){
+	  let html =
+      '<div class="punch-starter-wrapper">' +
+          '<div class="punch-starter-caption">' +
+            `<label>${punchStarter.name}</label>` +
+            `<label>${punchStarter.constructor.name}</label>` +
+          '</div>' +
+          '<div class="punch-starter-resume"></div>' +
+            `<label>Manufacturer</label>` +
+            `<p>${punchStarter.manufacturer}</p>` +
+            `<label>Description</label>` +
+            `<p>${punchStarter.description}</p>` +
+          '<div class="punch-starter-lists"></div>' +
+          '<div class="punch-starter-progress"></div>' +
+      '</div>'
+
+        $('.wrapper main').html(html)
+    }
 }
 
 module.exports = PunchStarterModel;
