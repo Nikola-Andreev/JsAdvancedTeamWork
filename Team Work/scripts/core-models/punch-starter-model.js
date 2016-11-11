92c9a2fdc6a1e30dfe9eba0f194d792745781197
Kamen <kamenminkov.com@abv.bg>
2016-11-11 20:45:08 +0200

    list items

--- Team Work/scripts/core-models/punch-starter-model.js
+++ Team Work/scripts/core-models/punch-starter-model.js
@@ -32,6 +32,70 @@ class PunchStarterModel {
         $('.punch-starter-progress').html(html)
         $('.progress-bar-inner').css('width', (progress < 100 ? (progress *0.7) + 'vw' : '70vw'))
     }
+
+    renderLists (punchStarter) {
+        let html = $('<div>' +
+            '<label>Genres</label>' +
+            '<ul><ul>' +
+            '</div>')
+        for (let row of punchStarter.genres){
+            html.append($('<li>').text(row))
+        }
+        if (punchStarter.constructor.name == 'CraftsPunchStarter'){
+            let inside = $('<div>' +
+                '<label>Resources</label>' +
+                '<ul></ul>'+
+                '</div>')
+            for (let element of punchStarter.resources){
+                inside.find('<ul>').append($('<li>').text(element))
+            }
+            html.append(inside)
+        }
+        if (punchStarter.constructor.name == 'FoodPunchStarter'){
+            let inside = $('<div>' +
+                '<label>Ingredients</label>' +
+                '<ul></ul>'+
+                '</div>')
+            for (let element of punchStarter.ingredients){
+                inside.find('<ul>').append($('<li>').text(element))
+            }
+            let recipe = $('<div>' +
+                '<label>Recipe</label>' +
+                '</div>')
+            recipe.append($('<p>').text(punchStarter.recipe))
+            html.append(inside)
+            html.append(recipe)
+        }
+
+        if (punchStarter.constructor.name == 'GamePunchStarter'){
+            let inside = $('<div>'+
+                '<label>Technologies Used</label>'+
+                '<ul></ul>' +
+                '</div>')
+            for (let row of punchStarter.technologiesUsed){
+                inside.find('<ul>').append($('<li>').text(row))
+            }
+            html.append(inside)
+        }
+
+
+        if (punchStarter.constructor.name == 'MoviePunchStarter'){
+            let inside = $('<div>' +
+                '<label>Actors</label>' +
+                '<ul></ul>'+
+                '</div>')
+            for (let element of punchStarter.actors){
+                inside.find('<ul>').append($('<li>').text(element))
+            }
+            let director = $('<div>' +
+                '<label>Director</label>' +
+                '</div>')
+            director.append($('<p>').text(punchStarter.director))
+            html.append(inside)
+            html.append(director)
+        }
+        $('.punch-starter-lists').html(html)
+    }
 }
 
 module.exports = PunchStarterModel;
