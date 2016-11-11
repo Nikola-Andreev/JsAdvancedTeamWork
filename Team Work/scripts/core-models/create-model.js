b978b3b7ecafba2f86a3a56853fb0bbb4cc2181e
Nikola Andreev <noka@abv.bg>
2016-11-11 14:35:40 +0200

    4.	The category property - Done

--- Team Work/scripts/core-models/create-model.js
+++ Team Work/scripts/core-models/create-model.js
@@ -1,5 +1,17 @@
 class CreateModel {
-    //TODO: Implement Create Model
+
+    constructor(){
+        this._category = 'Movie'
+    }
+
+    get category(){
+        return this._category
+    }
+
+    set category(cat){
+        this._category = cat
+        this.value = cat
+    }
 }
 
 module.exports = CreateModel;
