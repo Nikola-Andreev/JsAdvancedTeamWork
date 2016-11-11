c3a440e68b2cbcfd721138876361e7c52985fd34
Nikola Andreev <noka@abv.bg>
2016-11-11 20:40:00 +0200

    4.	Progress Bar

--- Team Work/scripts/core-models/punch-starter-model.js
+++ Team Work/scripts/core-models/punch-starter-model.js
@@ -1,6 +1,7 @@
 class PunchStarterModel {
 	render(punchStarter){
-	  let html =
+
+        let html =
       '<div class="punch-starter-wrapper">' +
           '<div class="punch-starter-caption">' +
             `<label>${punchStarter.name}</label>` +
@@ -16,6 +17,20 @@ class PunchStarterModel {
       '</div>'
 
         $('.wrapper main').html(html)
+    }
+
+    renderProgress(punchStarter){
+        let html =
+            '<p>Progress</p>' +
+            '<div class="donate-holder">' +
+            '<div class="progress-bar-outer">' +
+            `<div class="progress-bar-inner">${Math.round(Number(punchStarter.accumulatedMoney) / Number(punchStarter.targetPrice))}</div>` +
+            '</div>' +
+            '</div>' +
+            '<input type="number">' +
+            '<button>Donate</button>'
+        $('.punch-starter-progress').html(html)
+        $('.progress-bar-inner').css('width', (progress < 100 ? (progress *0.7) + 'vw' : '70vw'))
     }
 }
 
